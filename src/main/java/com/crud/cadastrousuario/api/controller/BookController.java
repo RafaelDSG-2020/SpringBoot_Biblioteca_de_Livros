32fdc1bc1c17445a0d5c878bce0dc9651ca0a180
Rafael Gonçalves <rafagomes53@alu.ufc.br>
2024-04-25 11:12:04 -0300

    Crud Book

--- src/main/java/com/crud/cadastrousuario/api/controller/BookController.java
+++ src/main/java/com/crud/cadastrousuario/api/controller/BookController.java
@@ -4,6 +4,7 @@ import com.crud.cadastrousuario.domain.dto.BookDTO;
 
 import com.crud.cadastrousuario.domain.service.CrudBookService;
 import jakarta.validation.Valid;
+import lombok.extern.log4j.Log4j2;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/books")
+@Log4j2
 public class BookController {
 
     @Autowired
@@ -29,8 +31,10 @@ public class BookController {
     @GetMapping
     public ResponseEntity<List<BookDTO>> findBookByParameters(@PageableDefault(size = 5) Pageable pageable, BookDTO filter) {
 
-        LOGGER.info("Method: findUserByParameters searches for a set of paginated books 5 by 5. HTTP Method: GET");
+        log.info("Method: findUserByParameters searches for a set of paginated books 5 by 5. HTTP Method: GET");
+        long start = System.currentTimeMillis();
         List<BookDTO> books = bookService.findUser(pageable , filter);
+        log.info("HTTP Method: GET Endpoint: api/v1/books  payload = {} elapsedTime = {} ms", filter , (System.currentTimeMillis() - start));
         return ResponseEntity.status(HttpStatus.OK).body(books);
 
     }
@@ -39,7 +43,9 @@ public class BookController {
     public ResponseEntity<Object> findBookByID(@PathVariable(value = "id") Long id){
 
         LOGGER.info("Method: findUserByID searches for just one book by their ID. HTTP Method: GET");
+        long start = System.currentTimeMillis();
         BookDTO bookSave = bookService.findBookByID(id);
+        log.info("HTTP Method: GET Endpoint: api/v1/books/{id}  payload = {} elapsedTime = {} ms", id , (System.currentTimeMillis() - start));
         return  ResponseEntity.status(HttpStatus.OK).body(bookSave);
 
     }
@@ -50,7 +56,9 @@ public class BookController {
     public ResponseEntity<Object> saveBook(@RequestBody @Valid BookDTO bookCreateDTO){
 
         LOGGER.info("Method: saveUser creates a book in the database. HTTP Method: POST");
+        long start = System.currentTimeMillis();
         BookDTO bookSave = bookService.save(bookCreateDTO);
+        log.info("HTTP Method: POST Endpoint: api/v1/books  payload = {} elapsedTime = {} ms", bookSave , (System.currentTimeMillis() - start));
         return ResponseEntity.status(HttpStatus.CREATED).body(bookSave);
     }
 
@@ -59,8 +67,9 @@ public class BookController {
                                              @RequestBody @Valid BookDTO bookCreateDTO) {
 
         LOGGER.info("Method: update User has the function of updating a book created in a table. HTTP Method: PUT");
-
+        long start = System.currentTimeMillis();
         BookDTO bookSave = bookService.updateBookByID(id, bookCreateDTO);
+        log.info("HTTP Method: PUT Endpoint: api/v1/books/{id}  payload = {} elapsedTime = {} ms", id , (System.currentTimeMillis() - start));
         return  ResponseEntity.status(HttpStatus.OK).body(bookSave);
 
     }
@@ -70,7 +79,9 @@ public class BookController {
     public ResponseEntity<Object> deleteUserByID(@PathVariable(value = "id") Long id) {
 
         LOGGER.info("Method: deleteUserByID deletes a book by ID in the database. HTTP Method: DELETE ");
+        long start = System.currentTimeMillis();
         bookService.deleteBookByID(id);
+        log.info("HTTP Method: DELETE Endpoint: api/v1/books/{id}  payload = {} elapsedTime = {} ms", id , (System.currentTimeMillis() - start));
         return ResponseEntity.noContent().build();
     }
 }

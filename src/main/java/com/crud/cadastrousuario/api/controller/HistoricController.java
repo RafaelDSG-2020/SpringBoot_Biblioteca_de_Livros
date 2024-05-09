7ea2bada55cf843d21b0b0a6f7c064dcd39cf2c4
Rafael Gonçalves <rafagomes53@alu.ufc.br>
2024-05-09 09:42:15 -0300

    findAll Atividade_10

--- src/main/java/com/crud/cadastrousuario/api/controller/HistoricController.java
+++ src/main/java/com/crud/cadastrousuario/api/controller/HistoricController.java
@@ -7,12 +7,13 @@ import com.crud.cadastrousuario.domain.service.HistoricService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Log4j2
 @RequestMapping("api/v1/histories")
@@ -23,6 +24,18 @@ public class HistoricController {
 
     @Autowired
     private HistoricService historicService;
+
+    @GetMapping
+    public ResponseEntity<List<HistoricDTO>> findHistoricsByParameters(@PageableDefault(size = 5) Pageable pageable, HistoricDTO filter) {
+
+        log.info("Method: findHistoricsByParameters searches for a set of paginated historics 5 by 5. HTTP Method: GET");
+        long start = System.currentTimeMillis();
+        List<HistoricDTO> historic = historicService.findHistoric(pageable , filter);
+        log.info("HTTP Method: GET Endpoint: api/v1/histories  payload = {} elapsedTime = {} ms", filter , (System.currentTimeMillis() - start));
+        return ResponseEntity.status(HttpStatus.OK).body(historic);
+
+    }
+
 
     @PostMapping("/{loanID}")
     public ResponseEntity<Object> saveHistoricLoan(@PathVariable Long loanID ){
--- src/main/java/com/crud/cadastrousuario/domain/repository/HistoricRepository.java
+++ src/main/java/com/crud/cadastrousuario/domain/repository/HistoricRepository.java
@@ -2,6 +2,7 @@ package com.crud.cadastrousuario.domain.repository;
 
 import com.crud.cadastrousuario.domain.model.Historic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface HistoricRepository extends JpaRepository<Historic, Long> {
+public interface HistoricRepository extends JpaRepository<Historic, Long> , JpaSpecificationExecutor<Historic> {
 }
